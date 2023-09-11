6790cd5ee562832e0ac394d59b9db6f7bc29294c
tasnimbinteyy <142822414+tasnimbinteyy@users.noreply.github.com>
2023-09-11 21:12:23 +0600

    Add files via upload

---
+++ Lab-1/Person1.java
@@ -0,0 +1,17 @@
+
+public class Person1 {
+    
+    public static void main(String[] args) {
+       Person nameage = new Person();
+       
+       // intialising the variables.
+       nameage.name = "Tasnim";
+       nameage.age = 20;
+       
+       //display the person name and age
+        System.out.println(" Person name = " +nameage.name);
+           System.out.println(" Person age = " +nameage.age);
+    }
+    
+    
+}
