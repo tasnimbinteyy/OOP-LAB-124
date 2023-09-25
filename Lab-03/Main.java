eb0292fbef3387110889023d74e7bd0165ecefb8
tasnimbinteyy <142822414+tasnimbinteyy@users.noreply.github.com>
2023-09-25 07:38:36 +0600

    Add files via upload

---
+++ Lab-03/Main.java
@@ -0,0 +1,11 @@
+
+package labreport;
+
+public class Main {
+    public static void main(String[] args) {
+        
+       Triangle obj = new Triangle();
+        System.out.println("Area = " +obj.getArea());
+       
+    }
+}
